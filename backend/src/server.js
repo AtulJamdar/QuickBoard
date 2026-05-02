84889c8aed1ae261ee59043f8d19ec20f81627e0
AtulJamdar <work.atuljamdar@gmail.com>
2026-05-02 17:09:28 +0530

    Database Connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const app = require('./app');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+
+
+connectDB().then(() => {
+    // Only start the server if the database connection is successful
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});

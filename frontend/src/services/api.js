7e464c9d65f9de88d8f2ad5874e25829050652f2
AtulJamdar <work.atuljamdar@gmail.com>
2026-05-04 00:02:04 +0530

    backend URI added in .env file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//The Base URL points to you Node.js server
 const API = axios.create({
-    baseURL: 'https://quickboard-production-f0ef.up.railway.app/api/v1',
+    // Uses the Vercel variable if it exists, otherwise defaults to local
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
 });
 
 export const createClass = (data) => API.post('/classes', data);
